ac49c27a32bab17cfc05f311b3b5d7e4535af40b
전윤민 <cordelia273@naver.com>
2024-01-08 03:48:05 +0900

    [UPDATE] show github link of project

--- components/projects/detail.tsx
+++ components/projects/detail.tsx
@@ -1,6 +1,7 @@
 import { Project } from "@/types";
 import { CustomMarkdown } from "../markdown/custom";
 import Image from "next/image";
+import Link from "next/link";
 
 export const ProjectDetail = (props: Project) => {
   const {
@@ -19,17 +20,38 @@ export const ProjectDetail = (props: Project) => {
 
   return (
     <div className="w-full p-2 md:p-2">
-      <header className="mb-4">
-        <h1 className="font-bold text-lg">사용기술</h1>
-      </header>
-      {skills.map((skill) =>
-        skill ? (
-          <div key={skill.id} className="flex items-center">
-            {skill.name}
-          </div>
-        ) : null
-      )}
+      <section className="mb-4">
+        <header className="mb-4">
+          <h1 className="font-bold text-lg">사용기술</h1>
+        </header>
+        {skills.map((skill) =>
+          skill ? (
+            <div key={skill.id} className="flex items-center">
+              {skill.name}
+            </div>
+          ) : null
+        )}
+      </section>
       <hr className="my-4" />
+
+      {github_link ? (
+        <>
+          <section className="mb-4">
+            <header className="mb-4">
+              <h1 className="font-bold text-lg">깃허브</h1>
+            </header>
+            <Link
+              href={github_link}
+              target="_blank"
+              className="text-violet-500 hover:text-violet-800 transition"
+            >
+              {github_link}
+            </Link>
+          </section>
+          <hr className="my-4" />
+        </>
+      ) : null}
+
       <header className="mb-4">
         <h1 className="font-bold text-lg">작품소개</h1>
       </header>
